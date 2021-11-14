6c8e5c8a78f36cb037283dc99a9993df354b44e3
akidane23 <arsemakidan@gmail.com>
2021-11-13 20:40:31 -0500

    updated code

--- db/connection.js
+++ db/connection.js
@@ -5,11 +5,11 @@ require("dotenv").config();
 // Connect to database
 const db = mysql.createConnection(
     {
-        host: process.env.HOST,
+        host: process.env.DB_HOST,
         // Your MySQL username,
         user: process.env.DB_USER,
         // Your MySQL password
-        password: process.env.PASS,
+        password: process.env.DB_PW,
         database: 'employee_tracker'
     },
     console.log('Connected to the employee_tracker database.')
--- db/seeds.sql
+++ db/seeds.sql
@@ -7,8 +7,8 @@ VALUES
 INSERT INTO roles (title, salary, department_id)
 VALUES
   ('Manager', 80000.00, 1),
-  ('Associate', 50000, 2),
-  ('Engineer', 650000, 3);
+  ('Associate', 60000, 2),
+  ('Engineer', 150000, 3);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
